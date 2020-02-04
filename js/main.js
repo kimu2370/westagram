cd2ceb0fd2b47c9566ae0e519f81d6a93ff5eef6
seunghyun <51603029+kimu2370@users.noreply.github.com>
2020-02-04 10:18:27 +0900

    addEvent comment-Delete

--- js/main.js
+++ js/main.js
@@ -5,24 +5,33 @@ let commentData = "";
 comment.addEventListener('change',(event)=>{
    if(event.target.value){
       commentData = event.target.value;
-      postFunction();
+      addPost();
    }
-   event.target.value = "";
+   event.target.value = ""; // 포커스를 떠나는 순간 value를 빈문자열로 바꾼다.
 });
+
 postButton.addEventListener('click',()=>{
    commentData = comment.value;
    if(commentData){
-      postFunction();
+      addPost();
    }
 });
 
-const postFunction = () => {
+const addPost = () => {
    const div = document.createElement('div');
    const span1 = document.createElement('span');
    const span2 = document.createElement('span');
+   let backUpData = "";
    span1.innerHTML = "k_seungHyun";
    span2.innerHTML = commentData;
    div.appendChild(span1);
    div.appendChild(span2);
    post.appendChild(div);
-};
+   const postUserName = document.querySelector('.post').querySelectorAll('div');
+   postUserName.forEach(item=>{  
+      item.addEventListener('click',()=>{    //댓글 삭제 기능
+         item.remove();
+         backUpData = post;
+      });
+   });
+}
